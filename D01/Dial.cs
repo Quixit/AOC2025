7ae19f3eb19ea5e0c376de2eaf539da74f352ea9
Jonathan Bruce <me@jonathanbruce.ca>
2025-12-04 22:18:38 -0500

    D01

---
+++ D01/Dial.cs
@@ -0,0 +1,40 @@
+namespace D01;
+
+public class Dial
+{
+    public int Value { get; set; } = 50;
+    
+    public int ZeroCount { get; set; } = 0;
+
+    public void Move(char direction, int magnitude)
+    {
+        var add = 0;
+
+        while (magnitude > 100)
+        {
+            magnitude -= 100;
+        }
+
+        switch (direction)
+        {
+            case 'L':
+                add -= magnitude;
+                break;
+            case 'R':
+                add += magnitude;
+                break;
+            default:
+                throw new NotSupportedException("Bad direction.");
+        }
+        
+        Value += add;
+        
+        if (Value < 0)
+            Value += 100;
+        else if (Value > 99)
+            Value -= 100;
+
+        if (Value == 0)
+            ZeroCount++;
+    }
+}
---
+++ D01/Program.cs
@@ -0,0 +1,12 @@
+﻿using D01;
+
+var input = File.ReadAllLines("input.txt").Select(line => (line[0],Convert.ToInt32(line.Substring(1)))).ToArray();
+var dial = new Dial();
+
+foreach (var line in input)
+{
+    dial.Move(line.Item1, line.Item2);
+}
+
+
+Console.WriteLine($"Result: {dial.ZeroCount}");
