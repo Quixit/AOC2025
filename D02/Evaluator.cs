b30975de0bda0730e0c50662412c74a7bd443c15
Quixit <me@jonathanbruce.ca>
2025-12-06 12:33:33 -0500

    Day 2

---
+++ D02/Evaluator.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+
+namespace D02;
+
+public class Evaluator
+{
+    public long InvalidTotal {
+        get;
+        set;
+    }
+
+    public void AddRange(Tuple<long,long> input)
+    {
+        var first = input.Item1;
+        var last = input.Item2;
+
+        Console.WriteLine($"Evaluating {first} to {last}");
+        
+        for (var i = first; i <= last; i++)
+        {
+            Add(i.ToString());
+        }
+    }
+
+    private void Add(string input)
+    {
+        if (input.Length % 2 == 0)
+        {
+            if (input.Substring(0, input.Length / 2) == input.Substring(input.Length / 2))
+            {
+                Console.WriteLine($"{input} Is Invalid");
+                InvalidTotal += Convert.ToInt64(input);
+            }
+        }
+
+        // for (var chunk = 1; chunk <= input.Length / 2; chunk++)
+        // {
+        //     for (var start = 0; start <= input.Length - chunk * 2; start++)
+        //     {
+        //         var first = input.Substring(start, chunk);
+        //         var second = input.Substring(start + chunk, chunk);
+        //
+        //         if (first == second)
+        //         {
+        //             Console.WriteLine($"  Found invalid chunk: {first} at position {start} of {input}");
+        //             InvalidTotal += Convert.ToInt64(input);
+        //             return;
+        //         }
+        //     }
+        // }
+    }
+}
---
+++ D02/Program.cs
@@ -0,0 +1,15 @@
+﻿using D02;
+
+var input = File.ReadAllText( "input.txt").Split(',').Select(i => i.Split('-').Select(x => Convert.ToInt64(x))).Select(y  =>
+{
+    var array = y.ToArray();
+    return new Tuple<long, long>(array[0], array[1]);
+});
+var evaluator = new Evaluator();
+
+foreach (var item in input)
+{
+    evaluator.AddRange(item);
+}
+
+Console.WriteLine($"Invalid Sum: {evaluator.InvalidTotal}");
