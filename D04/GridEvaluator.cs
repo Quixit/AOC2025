f1800f2e2ee7d43912bc5917642ffcb1460c6910
Quixit <me@jonathanbruce.ca>
2025-12-06 16:32:15 -0500

    Day 4

---
+++ D04/GridEvaluator.cs
@@ -0,0 +1,102 @@
+﻿using System.Reflection;
+
+namespace D04;
+
+public class GridEvaluator(bool[][] grid)
+{
+    public long Accessible
+    {
+        get;
+        set;
+    } = 0;
+
+    private int _maxX = grid.Length - 1;
+    private int _maxY = grid[0].Length - 1;
+
+    public void Evaluate(int x, int y)
+    {
+        //No roll at coordinates.
+        if (!grid[x][y])
+            return;
+
+        var count = 0;
+        
+        count += CheckUpLeft(x, y);
+        count += CheckUp(x, y);
+        count += CheckUpRight(x, y);
+        
+        count += CheckLeft(x, y);
+        count += CheckRight(x, y);
+        
+        count += CheckDownLeft(x, y);
+        count += CheckDown(x, y);
+        count += CheckDownRight(x, y);
+
+        if (count < 4)
+            Accessible++;
+    }
+
+    public int CheckUpLeft(int x, int y)
+    {
+        if (x == 0 || y == 0)
+            return 0;
+
+        return grid[x - 1][y - 1] ? 1: 0;
+    }
+    
+    public int CheckUp(int x, int y)
+    {
+        if (y == 0)
+            return 0;
+
+        return grid[x][y - 1] ? 1: 0;
+    }
+    
+    public int CheckUpRight(int x, int y)
+    {
+        if (x == _maxX || y == 0)
+            return 0;
+
+        return grid[x + 1][y - 1] ? 1: 0;
+    }
+    
+    public int CheckLeft(int x, int y)
+    {
+        if (x == 0)
+            return 0;
+
+        return grid[x - 1][y] ? 1: 0;
+    }
+    
+    public int CheckRight(int x, int y)
+    {
+        if (x == _maxX)
+            return 0;
+
+        return grid[x + 1][y] ? 1: 0;
+    }
+    
+    public int CheckDownLeft(int x, int y)
+    {
+        if (x == 0 || y == _maxY)
+            return 0;
+
+        return grid[x - 1][y + 1] ? 1: 0;
+    }
+    
+    public int CheckDown(int x, int y)
+    {
+        if (y == _maxY)
+            return 0;
+
+        return grid[x][y + 1] ? 1: 0;
+    }
+    
+    public int CheckDownRight(int x, int y)
+    {
+        if (x == _maxX || y == _maxY)
+            return 0;
+
+        return grid[x + 1][y + 1] ? 1: 0;
+    }
+}
---
+++ D04/Program.cs
@@ -0,0 +1,15 @@
+﻿using D04;
+
+var input = File.ReadAllLines("input.txt").Select(l => l.Select(s => s == '@').ToArray()).ToArray();
+
+var gridEvaluator = new GridEvaluator(input);
+
+for (var x = 0; x < input.Length; x++)
+{
+    for (var y = 0; y < input[x].Length; y++)
+    {
+        gridEvaluator.Evaluate(x, y);
+    }
+}
+
+Console.WriteLine($"Accessible roles: {gridEvaluator.Accessible}");
