5a69ba84a67823aff9f3ccd339d79c9d9915bfae
Quixit <me@jonathanbruce.ca>
2025-12-06 15:51:28 -0500

    Day 3

---
+++ D03/JoltageCalculator.cs
@@ -0,0 +1,47 @@
+﻿namespace D03;
+
+public class JoltageCalculator
+{
+    public long Total { get; set; } = 0;
+
+    public void AddBank(ArraySegment<long> bank, int values)
+    {
+        var total = "";
+        var slice = bank;
+        
+        for (var i = 1; i <= values; i++)
+        {
+            var highest = GetHighest(slice.Slice(0, slice.Count - values + i));
+            total += highest.Joltage.ToString();
+            
+            if ((highest.Index + 1) < slice.Count)
+                slice = slice.Slice(highest.Index + 1);
+        }
+
+        Console.WriteLine(total);
+        
+        Total += Convert.ToInt64(total);
+    }
+
+    public Battery GetHighest(ArraySegment<long> bank)
+    {
+        var highest = new Battery { Joltage = 0, Index = 0 };
+
+        for (var i = 0; i < bank.Count; i++)
+        {
+            // We want the first value that's the highest available, to give us more options for value two.
+            if (bank[i] > highest.Joltage)
+            {
+                highest = new Battery { Index = i, Joltage = bank[i] };
+            }
+        }
+
+        return highest;
+    }
+}
+
+public struct Battery
+{
+    public long Joltage { get; set; }
+    public int Index { get; set; }
+}
---
+++ D03/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using D03;
+
+var input = File.ReadAllLines("input.txt").Select(l => l.Select(s => Convert.ToInt64($"{s}")));
+var calculator = new JoltageCalculator();
+
+foreach (var bank in input)
+{
+    calculator.AddBank(new ArraySegment<long>(bank.ToArray()), 2);
+}
+
+Console.WriteLine($"Total Joltage: {calculator.Total}");
