e244268332b9bc7591f9d1aa70f2ae59063db382
Quixit <me@jonathanbruce.ca>
2025-12-13 15:10:30 -0500

    Day 6

--- D06/Program.cs
+++ D06/Program.cs
@@ -1,6 +1,5 @@
-﻿var input = File.ReadAllLines("input.txt");
-
-var updated = 1;
+﻿var raw = File.ReadAllLines("input.txt");
+var input = raw.Clone() as string[];
 
 for (var i = 0; i < input.Length; i++)
 {
@@ -13,28 +12,96 @@ for (var i = 0; i < input.Length; i++)
 var numbers = input[0..^1].Select(l => l.Trim().Split(' ').Select(s => long.Parse(s)).ToArray()).ToArray();
 var operators = input[^1].Split(" ");
 
-var sum = 0L;
+Console.WriteLine($"Sum: {GetSum(numbers)}");
 
-for (var col = 0; col < numbers[0].Length; col++)
+var numberGrid = raw[0..^1];
+var cephalapodNumbers = new List<long[]>();
+var currentValues = new List<long>();
+
+for (var x = 0; x < numberGrid[0].Length; x++)
 {
-    var operation = numbers[0][col];
-
-    for (var row = 1; row < numbers.Length; row++)
+   
+    var allSpace = true;
+    
+    var numberString = "";
+    
+    for (var y = 0; y < numberGrid.Length; y++)
     {
-        switch (operators[col])
-        {
-            case "+":
-                operation += numbers[row][col];
-                break;
-            case "*":
-                operation *= numbers[row][col];
-                break;
-            default:
-                throw new Exception($"Operator {operators[col]} not implemented");
+        if (numberGrid[y][x] != ' ')
+        {   
+            allSpace = false;
+            
+            numberString += numberGrid[y][x];
         }
     }
-    
-    sum += operation;
+
+    if (allSpace)
+    {
+        cephalapodNumbers.Add(currentValues.ToArray());
+        currentValues = new List<long>();
+    }
+    else
+    {
+        currentValues.Add(long.Parse(numberString.Trim()));
+    }
 }
 
-Console.WriteLine($"Sum: {sum}");
+cephalapodNumbers.Add(currentValues.ToArray());
+
+Console.WriteLine($"Cephalopod Sum: {GetCephalopodSum(cephalapodNumbers.ToArray())}");
+
+long GetSum(long[][] values) {
+    var sum = 0L;
+
+    for (var col = 0; col < values[0].Length; col++)
+    {
+        var operation = values[0][col];
+
+        for (var row = 1; row < values.Length; row++)
+        {
+            switch (operators[col])
+            {
+                case "+":
+                    operation += values[row][col];
+                    break;
+                case "*":
+                    operation *= values[row][col];
+                    break;
+                default:
+                    throw new Exception($"Operator {operators[col]} not implemented");
+            }
+        }
+    
+        sum += operation;
+    }
+
+    return sum;
+}
+
+long GetCephalopodSum(long[][] values) {
+    var sum = 0L;
+
+    for (var row = 0; row < values.Length; row++)
+    {
+        var operation = values[row][0];
+
+        for (var col = 1; col < values[row].Length; col++)
+        {
+            switch (operators[row])
+            {
+                case "+":
+                    operation += values[row][col];
+                    break;
+                case "*":
+                    operation *= values[row][col];
+                    break;
+                default:
+                    throw new Exception($"Operator {operators[col]} not implemented");
+            }
+        }
+    
+        sum += operation;
+    }
+
+    return sum;
+}
