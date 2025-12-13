40abe13a6e1f5f09d39d08f73810a49b474738a9
Quixit <me@jonathanbruce.ca>
2025-12-13 16:45:06 -0500

    Day 7

--- D07/Program.cs
+++ D07/Program.cs
@@ -3,35 +3,69 @@
 var raw = File.ReadAllLines("input.txt");
 var splits = 0L;
 
-var input = raw.Select(i => new StringBuilder(i)).ToArray();
+int start = -1, space = -2, splitter = -3;
+
+var input = raw.Select(i => i.Select(c =>
+{
+    switch (c)
+    {
+        case 'S':
+            return start;
+        case '.':
+            return space;
+        case '^':
+            return splitter;
+        default:
+            throw new ArgumentOutOfRangeException(nameof(c), c, null);
+    }
+}).ToArray()).ToArray();
 
 for (var y = 0; y < input.Length - 1; y++) // No need to process last row.
 {
     for (var x = 0; x < input[y].Length; x++)
     {
-        if (input[y][x] == '|' || input[y][x] == 'S')
+        if (input[y][x] != space && input[y][x] != splitter)
         {
-            if (input[y + 1][x] == '^')
+            var source = input[y][x] == start ? 1 : input[y][x];
+            
+            if (input[y + 1][x] == splitter)
             {
-                input[y + 1][x-1] = '|';
-                input[y + 1][x+1] = '|';
+                if (input[y + 1][x - 1] == space)
+                    input[y + 1][x - 1] = source;
+                else
+                    input[y + 1][x - 1] += source;
+                
+                
+                if (input[y + 1][x + 1] == space)
+                    input[y + 1][x + 1] = source;
+                else
+                    input[y + 1][x + 1] += source;
                 
                 splits++;
             }
-            else if (input[y + 1][x] == '.')
+            else if (input[y + 1][x] == space)
             {
-                input[y + 1][x] = '|';
+                input[y + 1][x] = source;
             }
-            else if (input[y + 1][x] == '|')
+            else if (input[y + 1][x] > 0)
             {
-                //Don't need to do anything.
-            }
-            else
-            {
-                throw new Exception("Unexpected input!");
+                input[y + 1][x] += source;
             }
         }
     }
 } 
 
+for (var y = 0; y < input.Length; y++) // No need to process last row.
+{
+    for (var x = 0; x < input[y].Length; x++)
+    {
+       Console.Write(input[y][x] + " ");
+    }
+    
+    Console.WriteLine();
+} 
+
+
+
 Console.WriteLine($"Splits: {splits}");
+Console.WriteLine($"Timelines: {input[^1].Where(v => v > 0).Sum()}");
